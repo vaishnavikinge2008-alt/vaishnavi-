955cd0fd4cda7b3515cd5b3b40ea4552ba1335b7
vaishnavikinge2008-alt <vaishnavikinge2008@gmail.com>
2026-09-21 12:58:28 +0530

    Create student performance.cpp

---
+++ student performance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int Roll numbers [5];
+
+cout " Enter roll Numbers : " endl;
+for ( int i=0; i< 5; i++) {
+cout << " student" << i + 1 << " : ";
+cin >> rolNumbers[i];
+}
+
+cout << "\n The roll numbers you entered arE;" << endl;
+for (int i =0; i < 5; i++) {
+cout << "student" << i+1 <<"
+Roll no: " << rollnumbers[i] << endl;
+}
+
+return 0;
+}
